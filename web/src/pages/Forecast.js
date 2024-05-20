08581601e3f87b809f0d1a6cc5ccda6e304bbb9d
heejicherry <jhj020320@ajou.ac.kr>
2024-05-20 15:00:54 +0900

    introduction page commit

--- web/src/pages/Forecast.js
+++ web/src/pages/Forecast.js
@@ -1,12 +1,21 @@
 import React from "react";
 import IndexNavbar from "../components/Navbars/CustomNavbar";
 import Dashboard from "views/Dashboard";
+import Predict from "../views/Predict";
+import Koreamap from "../views/Koreamap";
+import Search from "../views/Search";
+
 function Forecast() {
   return (
     <div>
       <IndexNavbar />
+      <Search />
+
+      <Predict />
 
       {/* <h2>Forecast 페이지</h2> */}
+
+      <Koreamap />
       <Dashboard />
     </div>
   );
