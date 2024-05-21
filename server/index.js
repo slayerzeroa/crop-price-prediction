1b695b2048819c565aa37047067810c84cc3e339
slayerzeroa <slayerzeroa@gmail.com>
2024-05-21 09:55:57 +0900

    fix: fix for nas

--- server/index.js
+++ server/index.js
@@ -7,12 +7,28 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 
+// // env\db\db_env.txt 파일에서 DB 정보 읽어오기
+// const fs = require("fs");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const db_env = fs
+//   .readFileSync(path.join(__dirname, "../env/db/db_env.txt"))
+//   .toString();
+// const db_env_list = db_env.split("\n");
+
+// const db_host = db_env_list[0].split("\r")[0];
+// const db_user = db_env_list[1].split("\r")[0];
+// const db_password = db_env_list[2].split("\r")[0];
+// const db_database = db_env_list[3].split("\r")[0];
+// //
+
 // mariadb 연결
 const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
+  host: "ajoufe.iptime.org",
+  user: "Ajou",
+  password: "0i!gx8D4uD5gRJdS",
   database: "aicapstone",
   connectionLimit: 5,
 });
@@ -29,7 +45,7 @@ app.get("/data", (req, res) => {
   const results = [];
   // mariadb에서 데이터 읽어오기
   pool.getConnection().then((conn) => {
-    conn.query("SELECT * FROM train").then((rows) => {
+    conn.query("SELECT * FROM crop").then((rows) => {
       rows.forEach((row) => {
         results.push(row);
       });
@@ -44,7 +60,7 @@ app.get("/price", (req, res) => {
   const results = [];
   // mariadb에서 데이터 읽어오기
   pool.getConnection().then((conn) => {
-    conn.query("SELECT ymd, price FROM train").then((rows) => {
+    conn.query("SELECT ymd, price FROM crop").then((rows) => {
       rows.forEach((row) => {
         results.push(row);
       });
@@ -60,7 +76,7 @@ app.get("/recent", (req, res) => {
   // mariadb에서 데이터 읽어오기
   pool.getConnection().then((conn) => {
     conn
-      .query("SELECT ymd, price FROM train ORDER BY ymd DESC LIMIT 30")
+      .query("SELECT ymd, price FROM crop ORDER BY ymd DESC LIMIT 30")
       .then((rows) => {
         rows.forEach((row) => {
           results.push(row);
